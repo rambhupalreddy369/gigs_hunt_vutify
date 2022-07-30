bf1853a15e73f1f6bf71b072041b33712bca19bc
rambhupal <rambhupalreddy369@gmail.com>
2022-07-30 18:57:15 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<div class="d-flex align-center">
+				
+			</div>
+
+			<v-spacer></v-spacer>
+
+			<v-btn
+			
+			>
+				<span class="mr-2"></span>
+				<v-icon></v-icon>
+			</v-btn>
+		</v-app-bar>
+
+		<v-main>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+	name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+	components: {
+	},
+
+	data: () => ({
+		//
+	}),
+};
+</script>
